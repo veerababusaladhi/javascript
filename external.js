abf2371875baa7c9a0df27d1d7c26a2e1602c432
veerababusaladhi <ganeshsaladhi6@gmail.com>
2023-08-24 04:27:09 +0530

    js files

---
+++ external.js
@@ -0,0 +1,56 @@
+a=20;
+document.write(a)
+
+
+//   var name='ganesh';
+// document.write(name.length);
+//   name = name + 'vijay';
+
+
+// var a=geetha peetha&seetha;
+//  document.write(a);
+
+
+
+// var a='ganesh';
+// document.write(typeof(a));
+
+
+// var arr=['ganesh','vijay','siva',29];
+// console.log(typeof(arr));
+// document.write(arr,"</br>");
+// // console.log(arr);
+// document.write(arr[1]);
+// document.write(arr[2]);
+
+
+
+
+
+// var trainees=[{
+// 	'name':'hari',
+// 	'rollno':'3947',
+// 	'branch':'ece',
+// 	'percent':'99.9'
+// },
+// {
+// 	'name':'ganesh',
+// 	'rollno':'3003',
+// 	'branch':'ces',
+// 	'percent':'98.9'
+// }]
+// console.log(students[0].rollno,students[1].name,students[0].branch);
+// console.log(students[0].branch,students[0].percent,students[0].name);
+
+
+
+
+
+// for (var i=0;i<trainees.length;i++)
+// {
+//       for (var key in trainees[i])
+//       {
+//         document.write(trainees[i][key],)
+//       }
+//        document.write("<br>")
+//     }
---
+++ table_task.js
@@ -0,0 +1,21 @@
+
+var trainees=[{
+	'name':'hari',
+	'rollno':'3947',
+	'branch':'ece',
+	'percent':'99.9'
+},
+{
+	'name':'ganesh',
+	'rollno':'3003',
+	'branch':'ces',
+	'percent':'98.9'
+}]
+// for (var i=0;i<trainees.length;i++)
+// {
+//       for (var key in trainees[i])
+//       {
+//         document.write(trainees[i][key],)
+//       }
+//        document.write("<br>")
+//     }
